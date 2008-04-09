0c314923127ef8d6ab6daf9c1b3783e100a62af4
allen.porter <allen.porter@efcbfb3a-694a-0410-8737-850316c721dc>
2008-04-09 06:00:47 +0000

    Initial revision.

---
+++ trunk/model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python2.4
+#
+# Copyright 2008 Google Inc. All Rights Reserved.
+
+"""One-line documentation for peer module.
+
+A detailed description of peer.
+"""
+
+__author__ = 'aporter@google.com (Allen Porter)'
+
+import base64
+from google.appengine.ext import db
+
+class Torrent(db.Model):
+  # Base64 encoded "info_hash" argument uniquely identifying the torrent
+  info_hash = db.StringProperty(required=True)
+
+  # Total number of times the tracker has registered a completion
+  downloaded = db.IntegerProperty()
+
+class TorrentPeerEntry(db.Model):
+  # Base64 encoded "info_hash" argument identifying the torrent
+  torrent = db.Reference(Torrent)
+
+  # IP:port of the client
+  ip = db.StringProperty(required=True)
+  port = db.IntegerProperty(required=True)
+
+  # Client supplied identifier
+  peer_id = db.StringProperty(required=True)
+
+  # Total amount downloaded (probably bytes)
+  downloaded = db.IntegerProperty()
+
+  # Total amount uploaded (probably bytes)
+  uploaded = db.IntegerProperty()
+
+  # Last time we saw the client
+  last_datetime = db.DateTimeProperty(required=True)
